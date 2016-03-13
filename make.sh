172514fa1f7f8866950377007d4cb0b194c13b37
Benjamin Black <weepingwillowben@gmail.com>
2016-03-12 20:11:55 -0800

    fixed windows problem

--- make.sh
+++ make.sh
@@ -1,2 +1,8 @@
 SRC="backend_srs"
-g++ -std=c++11 -O2 -shared -o cpp_code.so -fPIC -I "../myheaders" -I $SRC"/x86" -I $SRC $SRC/c_codegen.cpp $SRC/x86/basic_plat.cpp $SRC/x86/basic_kernel.cpp $SRC/compcode.cpp $SRC/utility.cpp
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    MYHEADERS="../myheaders"
+else
+    MYHEADERS="../Visual Studio 2013/"
+fi
+
+g++ -std=c++11 -O2 -shared -o cpp_code.so -fPIC -I "$MYHEADERS" -I $SRC"/x86" -I $SRC $SRC/c_codegen.cpp $SRC/x86/basic_plat.cpp $SRC/x86/basic_kernel.cpp $SRC/compcode.cpp $SRC/utility.cpp
